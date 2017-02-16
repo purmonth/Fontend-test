f2fbd8b8ec959293eb3a41914243f8e77d910d87
DCBP <DCBP@DCBP-iMac-21in-1.local>
2017-02-16 18:22:38 +0800

    02/16 lastest

---
+++ JaveScript/JavaScript_practice.js
@@ -0,0 +1,20 @@
+var i = 1;
+for(i = 0;i < 10;i ++){
+	document.write(i + "   ");
+}
+
+function printInt(a) {
+	document.write(a);
+}
+
+function printNewline() {
+	document.write("<br>");
+}
+
+var j = 10;
+printNewline();
+while (j > 0){
+	printInt(j);
+	printNewline();
+	j--;
+}
