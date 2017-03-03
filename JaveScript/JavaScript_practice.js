89275624cf1de2e2dcd6f0e635847469715fe02f
DCBP <DCBP@DCBP-iMac-21in-1.local>
2017-03-03 18:26:02 +0800

    JavaScript practice

--- JaveScript/JavaScript_practice.js
+++ JaveScript/JavaScript_practice.js
@@ -9,9 +9,34 @@ function getBinary(anInteger){
             result = "0" +result
         }
         anInteger = anInteger >> 1
+        console.log(anInteger+" "+result+" "+shortResult);
     }
     return (shortResult)
 }
 
 
+function Clock(hours, minutes){
+    this.hours = hours;
+    this.minutes = minutes;
+    this.setTime = setTime;
+    this.displayTime = displayTime;
+}
 
+function setTime(hours, minutes){
+    this.hours = hours;
+    this.minutes = minutes;
+}
+
+function displayTime(){
+    var line = this.hours + " : " + this.minutes;
+    document.writeln("<HR>Time of clock: "+line);
+}
+
+
+function fsearch (aForm){
+    alert("Sorry, search function not yet impelemented");
+}
+
+function foptions (aForm){
+    alert("Sorry, no options available");
+}
