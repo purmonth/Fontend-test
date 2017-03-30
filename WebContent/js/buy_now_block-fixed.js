eaef3fdef2cd4eccfd173367b7b4e89ac0d2f611
DCBP <DCBP@DCBP-iMac-21in-1.local>
2017-03-30 16:56:50 +0800

    fixed buy now car && overview

--- WebContent/js/buy_now_block-fixed.js
+++ WebContent/js/buy_now_block-fixed.js
@@ -1,8 +1,12 @@
 $(window).scroll(function(){
-	if($(this).scrollTop() >= 500){
-		$(".BuyNowBlock").css("position","fixed").css("top","200px");
-	}
-	if($(this).scrollTop() < 500){
-		$(".BuyNowBlock").css("position","absolute").css("top","600px");
+	if($(this).scrollTop() >= 346){
+		
+		if($(this).scrollTop() >= 5350){
+			$(".BuyNowBlock").css("position","absolute").css("top","5700px");
+		}else{
+			$(".BuyNowBlock").css("position","fixed").css("top","346px");
+		}
+	}else{
+		$(".BuyNowBlock").css("position","absolute").css("top","726px");
 	}
 });
