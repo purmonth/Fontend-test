571b64f370793a38c51f7fde7138ae1551ba3fd1
Gen Black <gen.black@gmail.com>
2017-03-31 12:38:21 +0800

    Reformat the buy_now_block-fixed.js

--- WebContent/js/buy_now_block-fixed.js
+++ WebContent/js/buy_now_block-fixed.js
@@ -1,11 +1,16 @@
-$(window).scroll(function(){
-	if($(this).scrollTop() >= 346){
-		if($(this).scrollTop() >= 5350){
-			$(".BuyNowBlock").css("position","absolute").css("top","5700px");
-		}else{
-			$(".BuyNowBlock").css("position","fixed").css("top","346px");
-		}
-	}else{
-		$(".BuyNowBlock").css("position","absolute").css("top","726px");
-	}
+$(window).scroll(function()
+{
+    if ($(this).scrollTop() >= 346)
+    {
+        if ($(this).scrollTop() >= 5350)
+        {
+            $(".BuyNowBlock").css("position", "absolute").css("top", "5700px");
+        } else
+        {
+            $(".BuyNowBlock").css("position", "fixed").css("top", "346px");
+        }
+    } else
+    {
+        $(".BuyNowBlock").css("position", "absolute").css("top", "726px");
+    }
 });
