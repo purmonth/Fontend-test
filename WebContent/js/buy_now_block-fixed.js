e5a0092a19e0d9a08256516e7e157be79fc9ace1
DCBP <DCBP@DCBP-iMac-21in-1.local>
2017-04-06 17:25:17 +0800

    buy now block js modified

--- WebContent/js/buy_now_block-fixed.js
+++ WebContent/js/buy_now_block-fixed.js
@@ -1,15 +1,44 @@
+/**
 $(window).scroll(function()
 {
     if ($(this).scrollTop() >= 346)
     {
-        if ($(this).scrollTop() >= 5350)
+        if ($(this).scrollTop() >= 5200)
         {
-            $(".BuyNowBlock").css("position", "absolute").css("top", "5700px");
+            $(".BuyNowBlock").css("position", "absolute").css("top", "5550px");
         } else
         {
             $(".BuyNowBlock").css("position", "fixed").css("top", "346px");
         }
-    } else
+    } else 
+    {
+        $(".BuyNowBlock").css("position", "absolute").css("top", "726px");
+    }
+});
+**/
+
+$(window).scroll(function()
+{
+    var distance = $(document).height()-1750;
+    /**
+    * 1750 px equal  
+    * footer height 800px  plus 
+    * plus buynowblock height 600px
+    */
+    if ($(this).scrollTop() >= 346)
+    {
+        
+        if ($(this).scrollTop() >= distance)
+        {
+            $(".BuyNowBlock").css("position", "absolute").css("top", distance+300)
+            /*
+            buy now block / 2 == 300
+             */
+        } else
+        {
+            $(".BuyNowBlock").css("position", "fixed").css("top", "").css("bottom","100px");
+        }
+    } else 
     {
         $(".BuyNowBlock").css("position", "absolute").css("top", "726px");
     }
